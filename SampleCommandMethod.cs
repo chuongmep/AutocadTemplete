71c8b52d4f12d0c8781a226dd6069f325ac55487
chuongmep <31106432+chuongmep@users.noreply.github.com>
2021-01-07 15:38:07 +0700

    re

---
+++ SampleCommandMethod.cs
@@ -0,0 +1,24 @@
+﻿using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.EditorInput;
+using System.IO;
+
+[assembly: ExtensionApplication(null)]
+[assembly: CommandClass(typeof(CommandMethodTemplate.SampleCommandClass))]
+namespace CommandMethodTemplate
+{
+    public class SampleCommandClass
+    {
+        [CommandMethod("HelloCommandMethod")]
+        public static void SampleCommandMethod()
+        {
+            // Current doc
+            Document acDoc = Application.DocumentManager.MdiActiveDocument;
+
+            // Write to editor
+            acDoc.Editor.WriteMessage("Hello from a command method.");
+        }
+    }
+}
